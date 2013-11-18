0c9c4df57eeac44b0df019b0798b97fab3debf9d
BenoitPerrot <benoit.noe.perrot@gmail.com>
2013-11-18 09:29:40 +0100

    org/whitehole/infra/types/StringAsciiz: Read from ByteArray8, least-significant byte first.

--- src/org/whitehole/infra/types/StringAsciiz.java
+++ src/org/whitehole/infra/types/StringAsciiz.java
@@ -16,6 +16,34 @@ public class StringAsciiz {
 		return sb.toString();
 	}
 
+	static public String readLittleEndian(ByteArray8 x) {
+		StringBuilder sb = new StringBuilder();
+		if (x.getByte0() != 0) {
+			sb.append((char) x.getByte0());
+			if (x.getByte1() != 0) {
+				sb.append((char) x.getByte1());
+				if (x.getByte2() != 0) {
+					sb.append((char) x.getByte2());
+					if (x.getByte3() != 0) {
+						sb.append((char) x.getByte3());
+						if (x.getByte4() != 0) {
+							sb.append((char) x.getByte4());
+							if (x.getByte5() != 0) {
+								sb.append((char) x.getByte5());
+								if (x.getByte6() != 0) {
+									sb.append((char) x.getByte6());
+									if (x.getByte7() != 0)
+										sb.append((char) x.getByte7());
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return sb.toString();
+	}
+
 	static public int byteSize(String s) {
 		// Consider that s is an ASCII string, and add one for the terminating zero
 		return 1 + s.length();
