6aa3c860191035ac3bbf2f546cd90bcfdc717614
Benoit Perrot <benoit.noe.perrot@gmail.com>
2015-02-22 10:11:51 +0100

    binary/pe: Publish ExportAddressNameOrdinalTable.

--- src/org/whitehole/binary/pe/ExportAddressNameOrdinalTable.java
+++ src/org/whitehole/binary/pe/ExportAddressNameOrdinalTable.java
@@ -1,3 +1,33 @@
+// Copyright (c) 2014-2015, Benoit PERROT.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of the White Hole Project nor the names of its
+//       contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package org.whitehole.binary.pe;
 
 import java.util.ArrayList;
@@ -9,10 +39,18 @@ import org.whitehole.infra.types.StringAsciiz;
 import org.whitehole.infra.types.UInt16;
 import org.whitehole.infra.types.UInt32;
 
-class ExportAddressNameOrdinalTable {
+public class ExportAddressNameOrdinalTable {
 
-	final ArrayList<UInt32> _eat;
-	final TreeMap<String, Integer> _est;
+	private final ArrayList<UInt32> _eat;
+	private final TreeMap<String, Integer> _est;
+
+	public ArrayList<UInt32> getExportAddressTable() {
+		return _eat;
+	}
+
+	public TreeMap<String, Integer> getExportSymbolTable() {
+		return _est;
+	}
 
 	public ExportAddressNameOrdinalTable(ArrayList<UInt32> eat, TreeMap<String, Integer> est) {
 		_eat = eat;
